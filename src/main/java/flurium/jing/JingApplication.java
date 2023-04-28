a3dfa7709609dfa0235246382a438d337b2881b1
Roman Koshchei <romankoshchei@gmail.com>
2023-04-28 20:30:33 +0300

    init

---
+++ src/main/java/flurium/jing/JingApplication.java
@@ -0,0 +1,13 @@
+package flurium.jing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flurium/jing/JingApplicationTests.java
@@ -0,0 +1,13 @@
+package flurium.jing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
