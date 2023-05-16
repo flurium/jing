92a71594a411a085c9da3d41f7a8af6496c393a3
Roman Koshchei <romankoshchei@gmail.com>
2023-05-16 11:47:21 +0300

    jing user

--- src/main/java/flurium/jing/db/models/JingUser.java
+++ src/main/java/flurium/jing/db/models/JingUser.java
@@ -3,6 +3,8 @@ package flurium.jing.db.models;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Entity
 @Table(name = "jing_users")
@@ -12,4 +14,12 @@ public class JingUser {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
+
+    private String email;
+
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "roles", joinColumns = @JoinColumn(name = "user_id"))
+    private List<String> roles;
 }
